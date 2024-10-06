eb6804dd39a9c730c9d57759becabd82003fc14e
nika todua <alchimikos@gmail.com>
2024-10-06 11:13:01 +0400

    update code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,31 +3,54 @@ using System.Collections.Generic;
 
 class RubiksCube
 {
-    static string[] moves = { "U", "U'", "D", "D'", "L", "L'", "R", "R'", "F", "F'", "B", "B'" };
+    // Array of possible moves for the Rubik's cube
+    static string[] moves = { "U", "U'","U2", "D", "D'", "D2", "L", "L'", "L2", "R", "R'", "R2", "F", "F'", "F2", "B", "B'", "B2" };
 
     static void Main(string[] args)
     {
-        int scrambleMoves = 20; // Number of random moves to scramble the cube
+        // Number of random moves to generate
+        int scrambleMoves = 20;
+
+        // Generate a list of scramble moves
         List<string> scrambleSequence = GenerateScramble(scrambleMoves);
 
+        // Display the scramble sequence
         Console.WriteLine("Scramble Sequence:");
-        foreach (var move in scrambleSequence)
+        foreach (string move in scrambleSequence)
         {
-            Console.Write(move + " ");
+            Console.Write(move + " "); // Print each move
         }
+        Console.WriteLine(); // New line after printing all moves
     }
 
-    static List<string> GenerateScramble(int moveCount)
+    // Function to generate the scramble sequence
+    static List<string> GenerateScramble(int numberOfMoves)
     {
-        Random random = new Random();
-        List<string> scramble = new List<string>();
+        Random random = new Random(); // Random number generator
+        List<string> scramble = new List<string>(); // List to store the scramble moves
+        string lastMove = ""; // Variable to track the last move
 
-        for (int i = 0; i < moveCount; i++)
+        // Loop through and generate the scramble moves
+        for (int i = 0; i < numberOfMoves; i++)
         {
-            int moveIndex = random.Next(moves.Length);
-            scramble.Add(moves[moveIndex]);
+            // Pick a random move
+            string move = moves[random.Next(moves.Length)];
+
+            // If the new move is on the same face as the last move, pick a different one
+            if (i > 0 && move[0] == lastMove[0])
+            {
+                // Pick a new move that is different from the last one
+                move = moves[random.Next(moves.Length)];
+            }
+
+            // Add the move to the list
+            scramble.Add(move);
+
+            // Update the last move
+            lastMove = move;
         }
 
+        // Return the list of moves
         return scramble;
     }
 }
